7431aef3da6d1fde943b21de7fba1e659f864354
Dillon <djpeters2002@gmail.com>
2026-07-09 22:55:43 -0500

    pages application creation

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Self Created
+
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
