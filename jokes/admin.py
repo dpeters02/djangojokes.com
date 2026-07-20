80be4f6946281853d77f10d97aad0cb1205ef74d
Dillon <djpeters2002@gmail.com>
2026-07-19 22:31:38 -0500

    joke model implementation to admin

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Joke
 
-# Register your models here.
+@admin.register(Joke)
+class JokeAdmin(admin.ModelAdmin):
+    model = Joke
+    list_display = ['question', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('created', 'updated')
+        
+        return ()
