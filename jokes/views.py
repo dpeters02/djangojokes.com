7976f717006819716d6aab4b4be19972976fd260
Dillon <djpeters2002@gmail.com>
2026-08-02 09:29:56 -0500

    modelform update

--- jokes/views.py
+++ jokes/views.py
@@ -1,10 +1,11 @@
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 from .models import Joke
 from django.urls import reverse_lazy
+from .forms import JokeForm
 
 class JokeCreateView(CreateView):
     model = Joke
-    fields = ['question', 'answer']
+    form_class = JokeForm
 
 class JokeDeleteView(DeleteView):
     model = Joke
@@ -19,6 +20,6 @@ class JokeListView(ListView):
 
 class JokeUpdateView(UpdateView):
     model = Joke
-    fields = ['question', 'answer']
+    form_class = JokeForm
 
 
