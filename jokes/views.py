c89cbc6e8a529d20dd21a27fc46b2f3cba7738d6
Dillon <djpeters2002@gmail.com>
2026-08-09 09:54:07 -0500

    Joke deletion

--- jokes/views.py
+++ jokes/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse_lazy
 from .forms import JokeForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin 
+from django.contrib import messages
 
 class JokeCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = Joke
@@ -18,10 +19,18 @@ class JokeDeleteView(UserPassesTestMixin, DeleteView):
     model = Joke
     success_url = reverse_lazy('jokes:list')
 
+    def delete(self, request, *args, **kwargs):
+        result = super().delete(request, *args, **kwargs)
+        return result
+
     def test_func(self):
         obj = self.get_object()
         return self.request.user ==obj.user
 
+    def form_valid(self, form):
+        messages.success(self.request, 'Joke Deleted')
+        return super().form_valid(form)
+    
 class JokeDetailView(DetailView):
     model = Joke
 
