a7d7e5a44a30582f140fba690b52c686301657eb
Dillon <djpeters2002@gmail.com>
2026-08-31 06:22:03 -0500

    getting joke ratings

--- jokes/models.py
+++ jokes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.conf import settings
+from django.db.models import Avg
 
 from templates.common.utils.text import unique_slug
 
@@ -31,6 +32,16 @@ class Joke(models.Model):
     @property
     def num_dislikes(self):
         return self.jokevotes.filter(vote=-1).count()
+
+    @property
+    def rating(self):
+        if self.num_votes == 0: # No jokes, so rating is 0
+            return 0
+
+        r = JokeVote.objects.filter(joke=self).aggregate(average=Avg('vote'))
+
+        # Return the rounded rating.
+        return round(5 + (r['average'] * 5), 2)
     
 
     def get_absolute_url(self):
